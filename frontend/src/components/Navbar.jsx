1e45dcb7d5be06d038acc25be59f944b675497cc
thenameisritika <ritikav2227@gmail.com>
2026-03-27 16:55:35 +0530

    added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,30 +2,24 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="bg-orange-500 px-6 py-4 flex justify-between items-center shadow-md">
+    
+     <nav className='bg-gray-950 flex px-6 py-6  justify-between items-center'>
 
-      {/* Logo */}
-      <div className="text-white text-2xl font-bold">
+       {/* Logo */}
+      <div className="text-yellow-400 text-2xl font-bold"> 
         🍛 Spice Garden
       </div>
 
       {/* Links */}
-      <div className="flex gap-6">
-        <Link to="/" className="text-white font-medium hover:text-orange-200">
-          Home
-        </Link>
-        <Link to="/menu" className="text-white font-medium hover:text-orange-200">
-          Menu
-        </Link>
-        <Link to="/order" className="text-white font-medium hover:text-orange-200">
-          Order Now
-        </Link>
-        <Link to="/admin" className="text-white font-medium hover:text-orange-200">
-          Admin
-        </Link>
+      <div className='flex gap-6'>
+        <Link  to="/" className='text-white hover:text-yellow-400'>Home</Link>
+        <Link  to="/menu" className='text-white hover:text-yellow-400'>Menu</Link>
+        <Link  to="/order" className='text-white hover:text-yellow-400'>Order</Link>
+         <Link to="/admin" className='text-white hover:text-yellow-400'>Admin</Link>
       </div>
 
-    </nav>
+     </nav>
+      
   )
 }
 
